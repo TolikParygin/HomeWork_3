b955451f4540d76263039ede3fbd45a786030042
Tolik <anatolijparygin988@gmail.com>
2022-12-03 19:47:55 +0300

    возведение в куб от 1 до N

---
+++ Task_3/Program.cs
@@ -0,0 +1,18 @@
+﻿// // Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// // 3 -> 1, 8, 27
+// // 5 -> 1, 8, 27, 64, 125
+Console.Clear();
+void FindCube(int N)
+{
+    int count = 1;
+    Console.Write(N + " -> ");
+    while (count <= N)
+    {
+        Console.Write(Math.Pow(count, 3) + "  ");
+        count++;
+    }
+}
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+FindCube(number);
